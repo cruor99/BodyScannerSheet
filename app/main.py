051c2af5876f4686625f8dd16ad5a7e353541a76
piees <yngveranestad@gmail.com>
2016-05-01 17:43:07 +0200

    init app with navigation

---
+++ app/main.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+from kivy.app import App
+from kivy.properties import ObjectProperty
+from kivy.uix.boxlayout import BoxLayout
+from kivy.metrics import dp
+
+# kivymd
+from kivymd.button import MDIconButton
+from kivymd.theming import ThemeManager
+from kivymd.navigationdrawer import NavigationDrawer
+
+class BodyScanRoot(BoxLayout):
+	pass
+
+class BodyScanNavDrawer(NavigationDrawer):
+	pass
+
+class BodyScanApp(App):
+	theme_cls = ThemeManager()
+	nav_drawer = ObjectProperty()
+
+	def build(self):
+		rootwidget = BodyScanRoot()
+		self.nav_drawer = BodyScanNavDrawer()
+
+
+if __name__ == "__main__":
+	BodyScanApp().run()
